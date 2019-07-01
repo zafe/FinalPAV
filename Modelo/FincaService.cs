4f25d09ccb8ee55184ca0efece9b16e07afcfe52
fernando <fernando@gmail.com>
2019-06-30 21:39:02 -0300

    Primer commit

---
+++ Modelo/FincaService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class FincaService : AbstractService<FincaSet>
+    {
+        public override void addEntity(FincaSet entity)
+        {
+            em.FincaSet.Add(entity);
+            em.SaveChanges();
+        }
+
+        public override void delEntity(object pk)
+        {
+            FincaSet finca = em.FincaSet.Where(q => q.Id == (int) pk).First<FincaSet>();
+            if (finca == null)
+            {
+                throw new ArgumentException("Finca no encontrada");
+            }
+            else
+            {
+                em.FincaSet.Remove(finca);
+                em.SaveChanges();
+            }
+        }
+
+        public override List<FincaSet> getEntities()
+        {
+            return em.FincaSet.ToList<FincaSet>();
+        }
+
+        public override FincaSet getEntity(object pk)
+        {
+            FincaSet finca = em.FincaSet.Where(q => q.Id == (int)pk).First<FincaSet>(); 
+            return finca ?? throw new ArgumentException("Finca no encontrada");
+        }
+
+        public override void updEntity(FincaSet entity)
+        {
+            FincaSet finca = em.FincaSet.Where(q => q.Id == (int) entity.Id).First<FincaSet>();
+            if (finca == null)
+            {
+                throw new ArgumentException("Finca no encontrada");
+            }
+            else
+            {
+                finca.Nombre = entity.Nombre;
+                em.SaveChanges();
+            }
+        }
+    }
+}
