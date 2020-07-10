22c86c1de06fdb0f7240cc1d7513cf70c738fa0e
Fernando <fernandozafe@gmail.com>
2020-07-10 02:17:45 -0300

    CV-3: Avances

---
+++ FinalPAV/ViewModel/ConductorABMViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FinalPAV.Utility;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FinalPAV.ViewModel
+{
+    class ConductorABMViewModel : IConductoreABM
+    {
+        public ICommand SaveCommand { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Persona SelectedPersona { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ConductorABMViewModel() {
+
+            SaveCommand = new CustomCommand(SaveConductor, CanSaveConductor);
+
+            Messenger.Default.Register<Persona>(this, OnPersonaReceived);
+
+            }
+
+        private void OnPersonaReceived(Persona personaReceived)
+        {
+            SelectedPersona = personaReceived;
+        }
+
+        private bool CanSaveConductor(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveConductor(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalPAV/ViewModel/IConductorABM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Model;
+
+namespace FinalPAV.ViewModel
+{
+    public interface IConductoreABM
+    {
+
+        ICommand SaveCommand { get; set; }
+        event PropertyChangedEventHandler PropertyChanged;
+        Persona SelectedPersona { get; set; }
+    }
+}
